be24e2a0ba3582f5a4b7bd68cd9de8966934036c
Fazlul <fkarif.cse@gmail.com>
2020-03-05 22:49:00 +0100

    Get and Display JSON Data with Fetch and Axios

---
+++ Get and Display JSON Data with Fetch and Axios/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <weather />
+  </div>
+</template>
+
+<script>
+import weather from "@/components/weather.vue";
+export default {
+  components: {
+    weather
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ Get and Display JSON Data with Fetch and Axios/src/components/weather.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h2>Weather App</h2>
+    <button @click="getWeatherList">Get data</button>
+    <div v-for="weatherdata in weatherList" :key="weatherdata.id">
+      <p>{{ weatherdata.title }}/p></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "weater",
+  data() {
+    return {
+      weatherList: []
+    };
+  },
+  methods: {
+    getWeatherList() {
+      //   fetch("https://jsonplaceholder.typicode.com/posts")
+      //     .then(response => response.json())
+      //     .then(data => (this.weatherList = data));
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => (this.weatherList = response.data));
+    }
+  }
+};
+</script>
+
+<style scoped></style>
